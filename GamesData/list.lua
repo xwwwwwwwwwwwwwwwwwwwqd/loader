f6c3d2dbebc31e626e62c15441479640e88667df
xwwwwwwwwwwwwwwwwwwwqd <abierockzoor@gmail.com>
2025-09-02 18:45:24 +0700

    Add files via upload

---
+++ GamesData/list.lua
@@ -0,0 +1,685 @@
+local Fishes = {
+	["Reef Chromis"] = {
+		Tier = "1",
+		Icon = "rbxassetid://130761855658346",
+		Probability = 0.5
+	},
+	["Abyss Seahorse"] = {
+		Tier = "5",
+		Icon = "rbxassetid://140212951494890",
+		Probability = 0.000010526315789473684
+	},
+	["Ash Basslet"] = {
+		Tier = "1",
+		Icon = "rbxassetid://89467763428838",
+		Probability = 0.5
+	},
+	["Astra Damsel"] = {
+		Tier = "4",
+		Icon = "rbxassetid://80597542368624",
+		Probability = 0.0005
+	},
+	["Azure Damsel"] = {
+		Tier = "1",
+		Icon = "rbxassetid://82037285284063",
+		Probability = 0.25
+	},
+	["Banded Butterfly"] = {
+		Tier = "2",
+		Icon = "rbxassetid://108599906664305",
+		Probability = 0.008
+	},
+	["Blue Lobster"] = {
+		Tier = "4",
+		Icon = "rbxassetid://79569787883962",
+		Probability = 0.00004
+	},
+	["Blueflame Ray"] = {
+		Tier = "5",
+		Icon = "rbxassetid://113303332536600",
+		Probability = 0.00001075268817204301
+	},
+	["Boa Angelfish"] = {
+		Tier = "1",
+		Icon = "rbxassetid://132840818442941",
+		Probability = 0.06666666666666667
+	},
+	["Bumblebee Grouper"] = {
+		Tier = "4",
+		Icon = "rbxassetid://138718017852833",
+		Probability = 0.0002
+	},
+	["Candy Butterfly"] = {
+		Tier = "2",
+		Icon = "rbxassetid://118627817589849",
+		Probability = 0.0026666666666666666
+	},
+	["Charmed Tang"] = {
+		Tier = "3",
+		Icon = "rbxassetid://115703943090504",
+		Probability = 0.003076923076923077
+	},
+	["Chrome Tuna"] = {
+		Tier = "4",
+		Icon = "rbxassetid://103021560011734",
+		Probability = 0.00011111111111111112
+	},
+	["Clownfish"] = {
+		Tier = "1",
+		Icon = "rbxassetid://98117815811053",
+		Probability = 0.5
+	},
+	["Coal Tang"] = {
+		Tier = "2",
+		Icon = "rbxassetid://92488575546885",
+		Probability = 0.02
+	},
+	["Copperband Butterfly"] = {
+		Tier = "2",
+		Icon = "rbxassetid://79518413256091",
+		Probability = 0.05
+	},
+	["Corazon Damsel"] = {
+		Tier = "1",
+		Icon = "rbxassetid://117026260439113",
+		Probability = 0.5
+	},
+	["Cow Clownfish"] = {
+		Tier = "4",
+		Icon = "rbxassetid://88952725333000",
+		Probability = 0.001
+	},
+	["Darwin Clownfish"] = {
+		Tier = "3",
+		Icon = "rbxassetid://109996187340520",
+		Probability = 0.0013333333333333333
+	},
+	["Domino Damsel"] = {
+		Tier = "4",
+		Icon = "rbxassetid://100965955122629",
+		Probability = 0.0006666666666666666
+	},
+	["Dorhey Tang"] = {
+		Tier = "4",
+		Icon = "rbxassetid://136891625122640",
+		Probability = 0.001
+	},
+	["Dotted Stingray"] = {
+		Tier = "5",
+		Icon = "rbxassetid://132465838670740",
+		Probability = 0.000010989010989010989
+	},
+	["Enchanted Angelfish"] = {
+		Tier = "4",
+		Icon = "rbxassetid://108347802265821",
+		Probability = 0.0002
+	},
+	["Fire Goby"] = {
+		Tier = "2",
+		Icon = "rbxassetid://94533805203782",
+		Probability = 0.004
+	},
+	["Firecoal Damsel"] = {
+		Tier = "4",
+		Icon = "rbxassetid://108870635586928",
+		Probability = 0.0004
+	},
+	["Flame Angelfish"] = {
+		Tier = "2",
+		Icon = "rbxassetid://128385926161840",
+		Probability = 0.01
+	},
+	["Greenbee Grouper"] = {
+		Tier = "4",
+		Icon = "rbxassetid://79015880866732",
+		Probability = 0.00016666666666666666
+	},
+	["Hammerhead Shark"] = {
+		Tier = "5",
+		Icon = "rbxassetid://91373020648344",
+		Probability = 0.00001000010000100001
+	},
+	["Hawks Turtle"] = {
+		Tier = "5",
+		Icon = "rbxassetid://129453304232199",
+		Probability = 0.000013333333333333333
+	},
+	["Starjam Tang"] = {
+		Tier = "4",
+		Icon = "rbxassetid://129447526017878",
+		Probability = 0.0002
+	},
+	["Jennifer Dottyback"] = {
+		Tier = "1",
+		Icon = "rbxassetid://82888113748973",
+		Probability = 0.5
+	},
+	["Jewel Tang"] = {
+		Tier = "2",
+		Icon = "rbxassetid://130344776739450",
+		Probability = 0.004
+	},
+	["Kau Cardinal"] = {
+		Tier = "3",
+		Icon = "rbxassetid://120735400809864",
+		Probability = 0.0013333333333333333
+	},
+	["Korean Angelfish"] = {
+		Tier = "3",
+		Icon = "rbxassetid://134484917450451",
+		Probability = 0.002857142857142857
+	},
+	["Lavafin Tuna"] = {
+		Tier = "4",
+		Icon = "rbxassetid://96111934641941",
+		Probability = 0.00010001000100010001
+	},
+	["Lobster"] = {
+		Tier = "4",
+		Icon = "rbxassetid://106973499045957",
+		Probability = 0.00004
+	},
+	["Loggerhead Turtle"] = {
+		Tier = "5",
+		Icon = "rbxassetid://91953712080535",
+		Probability = 0.000018181818181818182
+	},
+	["Longnose Butterfly"] = {
+		Tier = "4",
+		Icon = "rbxassetid://78488362036598",
+		Probability = 0.0006666666666666666
+	},
+	["Magic Tang"] = {
+		Tier = "4",
+		Icon = "rbxassetid://88573644059627",
+		Probability = 0.00013333333333333334
+	},
+	["Magma Goby"] = {
+		Tier = "2",
+		Icon = "rbxassetid://136836819813966",
+		Probability = 0.01818181818181818
+	},
+	["Manta Ray"] = {
+		Tier = "5",
+		Icon = "rbxassetid://88385941559907",
+		Probability = 0.00002
+	},
+	["Maroon Butterfly"] = {
+		Tier = "4",
+		Icon = "rbxassetid://0",
+		Probability = 0.001
+	},
+	["Maze Angelfish"] = {
+		Tier = "3",
+		Icon = "rbxassetid://133123622412997",
+		Probability = 0.008
+	},
+	["Moorish Idol"] = {
+		Tier = "4",
+		Icon = "rbxassetid://99527607304877",
+		Probability = 0.00030003000300030005
+	},
+	["Bandit Angelfish"] = {
+		Tier = "2",
+		Icon = "rbxassetid://86776001616210",
+		Probability = 0.015384615384615385
+	},
+	["Zoster Butterfly"] = {
+		Tier = "1",
+		Icon = "rbxassetid://80869011808607",
+		Probability = 0.125
+	},
+	["Orangy Goby"] = {
+		Tier = "1",
+		Icon = "rbxassetid://80870534603019",
+		Probability = 0.14285714285714285
+	},
+	["Panther Grouper"] = {
+		Tier = "4",
+		Icon = "rbxassetid://110559683316227",
+		Probability = 0.001
+	},
+	["Prismy Seahorse"] = {
+		Tier = "5",
+		Icon = "rbxassetid://105300029672501",
+		Probability = 0.000011363636363636363
+	},
+	["Scissortail Dartfish"] = {
+		Tier = "2",
+		Icon = "rbxassetid://90918658215983",
+		Probability = 0.0033333333333333335
+	},
+	["Shrimp Goby"] = {
+		Tier = "2",
+		Icon = "rbxassetid://79722493639079",
+		Probability = 0.02
+	},
+	["Skunk Tilefish"] = {
+		Tier = "1",
+		Icon = "rbxassetid://117649671337741",
+		Probability = 0.14285714285714285
+	},
+	["Specked Butterfly"] = {
+		Tier = "1",
+		Icon = "rbxassetid://116155560772333",
+		Probability = 0.5
+	},
+	["Strawberry Dotty"] = {
+		Tier = "1",
+		Icon = "rbxassetid://113189737934915",
+		Probability = 0.05
+	},
+	["Sushi Cardinal"] = {
+		Tier = "4",
+		Icon = "rbxassetid://104436232234256",
+		Probability = 0.0008
+	},
+	["Tricolore Butterfly"] = {
+		Tier = "2",
+		Icon = "rbxassetid://138769463668635",
+		Probability = 0.014285714285714285
+	},
+	["Unicorn Tang"] = {
+		Tier = "4",
+		Icon = "rbxassetid://85435653124474",
+		Probability = 0.00022222222222222223
+	},
+	["Vintage Blue Tang"] = {
+		Tier = "1",
+		Icon = "rbxassetid://136876564868613",
+		Probability = 0.5
+	},
+	["Vintage Damsel"] = {
+		Tier = "2",
+		Icon = "rbxassetid://135465556501628",
+		Probability = 0.007407407407407408
+	},
+	["Volcanic Basslet"] = {
+		Tier = "1",
+		Icon = "rbxassetid://73441603866106",
+		Probability = 0.5
+	},
+	["White Clownfish"] = {
+		Tier = "2",
+		Icon = "rbxassetid://94709558139061",
+		Probability = 0.004
+	},
+	["Yello Damselfish"] = {
+		Tier = "2",
+		Icon = "rbxassetid://125066072333378",
+		Probability = 0.01818181818181818
+	},
+	["Yellowfin Tuna"] = {
+		Tier = "4",
+		Icon = "rbxassetid://86565235696991",
+		Probability = 0.00013333333333333334
+	},
+	["Yellowstate Angelfish"] = {
+		Tier = "1",
+		Icon = "rbxassetid://82807541331151",
+		Probability = 0.5
+	},
+	["Salmon"] = {
+		Tier = "2",
+		Icon = "rbxassetid://125238936461158",
+		Probability = 0.02
+	},
+	["Blob Shark"] = {
+		Tier = "7",
+		Icon = "rbxassetid://120294742064292",
+		Probability = 0.000004
+	},
+	["Volsail Tang"] = {
+		Tier = "3",
+		Icon = "rbxassetid://129946055426678",
+		Probability = 0.0033333333333333335
+	},
+	["Rockform Cardianl"] = {
+		Tier = "3",
+		Icon = "rbxassetid://120329148296828",
+		Probability = 0.004
+	},
+	["Lava Butterfly"] = {
+		Tier = "2",
+		Icon = "rbxassetid://121441755541039",
+		Probability = 0.008
+	},
+	["Slurpfish Chromis"] = {
+		Tier = "2",
+		Icon = "rbxassetid://73157781682607",
+		Probability = 0.000125
+	},
+	["Festive Goby"] = {
+		Tier = "1",
+		Icon = "rbxassetid://131719468046777",
+		Probability = 0.3333333333333333
+	},
+	["Mistletoe Damsel"] = {
+		Tier = "1",
+		Icon = "rbxassetid://119896827350397",
+		Probability = 0.25
+	},
+	["Gingerbread Tang"] = {
+		Tier = "1",
+		Icon = "rbxassetid://81165959957452",
+		Probability = 0.2
+	},
+	["Great Christmas Whale"] = {
+		Tier = "7",
+		Icon = "rbxassetid://71302255255094",
+		Probability = 0.000001
+	},
+	["Gingerbread Clownfish"] = {
+		Tier = "2",
+		Icon = "rbxassetid://76627481663195",
+		Probability = 0.01818181818181818
+	},
+	["Gingerbread Turtle"] = {
+		Tier = "5",
+		Icon = "rbxassetid://105391647263950",
+		Probability = 0.000014285714285714285
+	},
+	["Gingerbread Shark"] = {
+		Tier = "6",
+		Icon = "rbxassetid://134485196164902",
+		Probability = 0.000005
+	},
+	["Christmastree Longnose"] = {
+		Tier = "3",
+		Icon = "rbxassetid://121538254026074",
+		Probability = 0.002857142857142857
+	},
+	["Candycane Lobster"] = {
+		Tier = "4",
+		Icon = "rbxassetid://81306607650028",
+		Probability = 0.0005
+	},
+	["Festive Pufferfish"] = {
+		Tier = "4",
+		Icon = "rbxassetid://111730193849173",
+		Probability = 0.0008333333333333334
+	},
+	["Blue-Banded Goby"] = {
+		Tier = "2",
+		Icon = "rbxassetid://137187691311291",
+		Probability = 0.02
+	},
+	["Blumato Clownfish"] = {
+		Tier = "2",
+		Icon = "rbxassetid://135433263876257",
+		Probability = 0.01818181818181818
+	},
+	["Conspi Angelfish"] = {
+		Tier = "1",
+		Icon = "rbxassetid://70666845671780",
+		Probability = 0.5
+	},
+	["Fade Tang"] = {
+		Tier = "1",
+		Icon = "rbxassetid://99713500880459",
+		Probability = 0.06666666666666667
+	},
+	["Lined Cardinal Fish"] = {
+		Tier = "5",
+		Icon = "rbxassetid://96973556889996",
+		Probability = 0.0001818181818181818
+	},
+	["Masked Angelfish"] = {
+		Tier = "1",
+		Icon = "rbxassetid://85192354177842",
+		Probability = 0.5
+	},
+	["Pygmy Goby"] = {
+		Tier = "1",
+		Icon = "rbxassetid://128173847216247",
+		Probability = 0.16666666666666666
+	},
+	["Sail Tang"] = {
+		Tier = "1",
+		Icon = "rbxassetid://86089485683202",
+		Probability = 0.2
+	},
+	["Watanabei Angelfish"] = {
+		Tier = "1",
+		Icon = "rbxassetid://133021124365377",
+		Probability = 0.25
+	},
+	["White Tang"] = {
+		Tier = "1",
+		Icon = "rbxassetid://138516719984773",
+		Probability = 0.2
+	},
+	["Ballina Angelfish"] = {
+		Tier = "3",
+		Icon = "rbxassetid://99236757363784",
+		Probability = 0.002857142857142857
+	},
+	["Bleekers Damsel"] = {
+		Tier = "2",
+		Icon = "rbxassetid://116977046936052",
+		Probability = 0.02857142857142857
+	},
+	["Loving Shark"] = {
+		Tier = "6",
+		Icon = "rbxassetid://119173750281399",
+		Probability = 0.000006666666666666667
+	},
+	["Pink Smith Damsel"] = {
+		Tier = "2",
+		Icon = "rbxassetid://76606007357699",
+		Probability = 0.02
+	},
+	["Axolotl"] = {
+		Tier = "4",
+		Icon = "rbxassetid://76558551981982",
+		Probability = 0.00015384615384615385
+	},
+	["Silver Tuna"] = {
+		Tier = "2",
+		Icon = "rbxassetid://84055400571306",
+		Probability = 0.016666666666666666
+	},
+	["Pilot Fish"] = {
+		Tier = "2",
+		Icon = "rbxassetid://114269547366510",
+		Probability = 0.02
+	},
+	["Patriot Tang"] = {
+		Tier = "1",
+		Icon = "rbxassetid://118534942671075",
+		Probability = 0.1
+	},
+	["Frostborn Shark"] = {
+		Tier = "7",
+		Icon = "rbxassetid://125463067542850",
+		Probability = 0.000002
+	},
+	["Plasma Shark"] = {
+		Tier = "5",
+		Icon = "rbxassetid://88847410678758",
+		Probability = 0.000004444444444444444
+	},
+	["Pufferfish"] = {
+		Tier = "4",
+		Icon = "rbxassetid://88950964359044",
+		Probability = 0.0006666666666666666
+	},
+	["Racoon Butterfly Fish"] = {
+		Tier = "2",
+		Icon = "rbxassetid://92680808637130",
+		Probability = 0.02
+	},
+	["Orange Basslet"] = {
+		Tier = "2",
+		Icon = "rbxassetid://129792505755834",
+		Probability = 0.02
+	},
+	["Strippled Seahorse"] = {
+		Tier = "5",
+		Icon = "rbxassetid://120444751052938",
+		Probability = 0.000010526315789473684
+	},
+	["Thresher Shark"] = {
+		Tier = "5",
+		Icon = "rbxassetid://70681751795899",
+		Probability = 0.000010526315789473684
+	},
+	["Great Whale"] = {
+		Tier = "7",
+		Icon = "rbxassetid://110349456710432",
+		Probability = 0.0000010526315789473683
+	},
+	["Worm Fish"] = {
+		Tier = "7",
+		Icon = "rbxassetid://88157067345872",
+		Probability = 3.3333333333333335e-07
+	},
+	["Viperfish"] = {
+		Tier = "2",
+		Icon = "rbxassetid://132331063285672",
+		Probability = 0.02
+	},
+	["Deep Sea Crab"] = {
+		Tier = "5",
+		Icon = "rbxassetid://107492220673765",
+		Probability = 0.0002
+	},
+	["Spotted Lantern Fish"] = {
+		Tier = "2",
+		Icon = "rbxassetid://121295544969455",
+		Probability = 0.02
+	},
+	["Robot Kraken"] = {
+		Tier = "7",
+		Icon = "rbxassetid://80927639907406",
+		Probability = 2.857142857142857e-07
+	},
+	["Monk Fish"] = {
+		Tier = "4",
+		Icon = "rbxassetid://117683281700675",
+		Probability = 0.0003333333333333333
+	},
+	["King Crab"] = {
+		Tier = "6",
+		Icon = "rbxassetid://136951139611035",
+		Probability = 8.333333333333333e-07
+	},
+	["Jellyfish"] = {
+		Tier = "3",
+		Icon = "rbxassetid://115477999636961",
+		Probability = 0.0033333333333333335
+	},
+	["Giant Squid"] = {
+		Tier = "7",
+		Icon = "rbxassetid://135470649745250",
+		Probability = 0.00000125
+	},
+	["Fangtooth"] = {
+		Tier = "4",
+		Icon = "rbxassetid://82285034746187",
+		Probability = 0.0005
+	},
+	["Electric Eel"] = {
+		Tier = "1",
+		Icon = "rbxassetid://112551199622795",
+		Probability = 0.5
+	},
+	["Vampire Squid"] = {
+		Tier = "4",
+		Icon = "rbxassetid://87203317027314",
+		Probability = 0.0003333333333333333
+	},
+	["Dark Eel"] = {
+		Tier = "2",
+		Icon = "rbxassetid://135880333888900",
+		Probability = 0.02
+	},
+	["Boar Fish"] = {
+		Tier = "1",
+		Icon = "rbxassetid://114009571858522",
+		Probability = 0.5
+	},
+	["Blob Fish"] = {
+		Tier = "6",
+		Icon = "rbxassetid://109329387304752",
+		Probability = 0.00002
+	},
+	["Angler Fish"] = {
+		Tier = "4",
+		Icon = "rbxassetid://95565358831204",
+		Probability = 0.0003333333333333333
+	},
+	["Dead Fish"] = {
+		Tier = "1",
+		Icon = "rbxassetid://72777968915255",
+		Probability = 0.25
+	},
+	["Skeleton Fish"] = {
+		Tier = "1",
+		Icon = "rbxassetid://110090152643341",
+		Probability = 0.1
+	},
+	["Swordfish"] = {
+		Tier = "2",
+		Icon = "rbxassetid://105539179190905",
+		Probability = 0.02
+	},
+	["Ghost Worm Fish"] = {
+		Tier = "7",
+		Icon = "rbxassetid://107896897205452",
+		Probability = 0.000001
+	},
+	["Ghost Shark"] = {
+		Tier = "7",
+		Icon = "rbxassetid://102028503382077",
+		Probability = 0.000002
+	},
+	["Rockfish"] = {
+		Tier = "2",
+		Icon = "rbxassetid://88961987133136",
+		Probability = 0.02
+	},
+	["Sheepshead Fish"] = {
+		Tier = "3",
+		Icon = "rbxassetid://112711438552296",
+		Probability = 0.0033333333333333335
+	},
+	["Blackcap Basslet"] = {
+		Tier = "2",
+		Icon = "rbxassetid://95659897738388",
+		Probability = 0.02
+	},
+	["Catfish"] = {
+		Tier = "3",
+		Icon = "rbxassetid://98970734819318",
+		Probability = 0.0033333333333333335
+	},
+	["Coney Fish"] = {
+		Tier = "3",
+		Icon = "rbxassetid://126049387807805",
+		Probability = 0.0033333333333333335
+	},
+	["Hermit Crab"] = {
+		Tier = "6",
+		Icon = "rbxassetid://84444153694943",
+		Probability = 0.000016666666666666667
+	},
+	["Parrot Fish"] = {
+		Tier = "2",
+		Icon = "rbxassetid://101895900955177",
+		Probability = 0.02
+	},
+	["Queen Crab"] = {
+		Tier = "7",
+		Icon = "rbxassetid://73709080100110",
+		Probability = 0.00000125
+	},
+	["Red Snapper"] = {
+		Tier = "2",
+		Icon = "rbxassetid://70818316245817",
+		Probability = 0.02
+	},
+
+}
+
+return Fishes
